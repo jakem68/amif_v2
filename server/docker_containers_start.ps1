4079f5b31f40201decd7001a37b41ebd16b61d35
Jan Kempeneers <jakem68@users.noreply.github.com>
2024-10-02 10:50:48 +0200

    Update docker_containers_start.ps1

--- server/docker_containers_start.ps1
+++ server/docker_containers_start.ps1
@@ -1,17 +1,20 @@
 docker stop portainer
 docker start portainer
-docker stop mosquitto
-docker rm mosquitto
-docker run -itd --name="mosquitto" -p 1883:1883 -p 9001:9001 --restart always -v c:/docker/mqtt_broker/mosquitto.conf:/mosquitto/config/mosquitto.conf -v mosquitto_data:/mosquitto/data -v mosquitto_data:/mosquitto/log eclipse-mosquitto
-docker stop mc-server
-docker rm mc-server
-docker run --name mc-server -v c:\docker\mqttcool\my-connector_conf.xml:/mqtt.cool/mqtt_connectors/mqtt_master_connector_conf.xml -d -p 8080:8080 mqttcool/mqtt.cool
+# docker stop mosquitto
+# docker rm mosquitto
+# docker run -itd --name="mosquitto" -p 1883:1883 -p 9001:9001 --restart always -v c:/docker/mqtt_broker/mosquitto.conf:/mosquitto/config/mosquitto.conf -v mosquitto_data:/mosquitto/data -v mosquitto_data:/mosquitto/log eclipse-mosquitto
+# docker stop mc-server
+# docker rm mc-server
+# docker run --name mc-server -v c:\docker\mqttcool\my-connector_conf.xml:/mqtt.cool/mqtt_connectors/mqtt_master_connector_conf.xml -d -p 8080:8080 mqttcool/mqtt.cool
 cd c:\docker\tig_stack
 docker-compose down
 docker-compose up -d
 cd C:\docker\mqtt_connections
 docker-compose down
 docker-compose up -d
-cd C:\docker\24vapi
+# cd C:\docker\24vapi
+# docker-compose down
+# docker-compose up -d
+cd C:\docker\ignition_stack
 docker-compose down
 docker-compose up -d
